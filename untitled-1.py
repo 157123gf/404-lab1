ea861a80b34f089d582820dda618c054f14ba2f5
157123gf <71798841+157123gf@users.noreply.github.com>
2023-09-13 18:24:26 -0600

    modified:   untitled-1.py

--- untitled-1.py
+++ untitled-1.py
@@ -2,6 +2,6 @@ import requests
 
 print(requests.__version__)
 
-resp=requests.get("http://google.com")
+resp=requests.get("https://raw.githubusercontent.com/157123gf/404-lab1/master/untitled-1.py")
 
 print(resp.text)
